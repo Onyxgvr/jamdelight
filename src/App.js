071b5c05f2ba6cfe91a8f0807a6245b4838f59d8
d3vi11ain <d3vi11ain@users.noreply.github.com>
2020-01-23 14:19:30 +0200

    Hello second commit!

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           First Change
+          Second Change
         </p>
         <a
           className="App-link"
